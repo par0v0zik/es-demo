4ba49ac9b28d83eccfdf03f44684bdfc86397b8d
Олег Пастушенко <oleg.pastushenko@saltpepper.ru>
2016-02-14 23:16:58 +0300

    Add Doctor model

---
+++ app/models/doctor.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+#
+# Table name: doctors
+#
+#  id            :integer          not null, primary key
+#  first_name    :string
+#  last_name     :string
+#  speciality_id :integer
+#  posts_count   :integer          default(0)
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
+class Doctor < ActiveRecord::Base
+end
